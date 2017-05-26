60fbdd32cb948bcb1f09bf718ea94ef0e92b1824
proverbs <379548839@qq.com>
2017-05-26 22:01:13 +0800

    commit with terminal

---
+++ POJ2185.h
@@ -0,0 +1,16 @@
+//
+// Created by proverbs on 5/26/17.
+//
+
+#ifndef POJ_POJ2185_H
+#define POJ_POJ2185_H
+
+#include <iostream>
+
+using namespace std;
+
+void solve() {
+    cout << "fuck" << endl;
+}
+
+#endif //POJ_POJ2185_H
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "POJ2185.h"
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    solve();
+    return 0;
+}
